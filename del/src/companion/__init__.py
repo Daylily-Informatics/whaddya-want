6e067c74089e472c4cc116e85d2f1c3b09105ec8
John Major <daylily@daylilyinformatics.com>
2025-11-26 03:08:26 -0800

    X

--- del/src/companion/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Runtime package for the AWS-hosted AI companion example."""
-from .broker import ConversationBroker
-from .config import RuntimeConfig
-
-__all__ = ["ConversationBroker", "RuntimeConfig"]
