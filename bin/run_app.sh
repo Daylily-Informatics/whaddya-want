8f852796e3bd64c54d3c2efe45cb132b38777a13
John Major <daylily@daylilyinformatics.com>
2025-11-29 09:19:10 -0800

    X

--- bin/run_app.sh
+++ bin/run_app.sh
@@ -6,7 +6,7 @@ ENABLE_OUTBOUND_EMAIL=1        # to actually send email
 ENABLE_SYSTEM_COMMANDS=1       # to allow run_command
 ACTION_EMAIL_FROM=john@dyly.bio  # verified in SES
 
-PYTHONPATH=. python -m client.cli --session $SESSION \
+PYTHONPATH=$PWD:$PWD/layers/shared/python python -m client.cli --session $SESSION \
   --broker-url $BROKER   \
   --setup-devices --voice Matthew --voice-mode generative --verbose -vv --self-voice-name Matthew 
 
